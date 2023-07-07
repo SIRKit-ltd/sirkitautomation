433c161add455f2cb6e38a254b9d3eaa003680a2
SIRKit-ltd <Licensing@SIRKit.ca>
2023-07-07 14:07:14 -0600

    Created test function to test SirkIT Helper
    Created test function to test SirkIT Helper funtions

---
+++ ClientTest/run.ps1
@@ -0,0 +1,25 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+#Setup a blank outputBody varible.
+$outputBody = ""
+
+#process input from the HTTP request and convert the allClients string value to a boolean.
+if ($Request.Body -is [Hashtable]) { $body = $Request.Body  } else { $body = $Request.Body | ConvertFrom-Json }
+$allClientsBool = $false
+if ($Request.Query.allClients -eq 'true') { $allClientsBool = $true }
+
+#Get a filtered client list of only clients that should have code executed.
+clientList = GetClientList -allClients $allClientsBool -excludedClients "$($body.excludedClients)" -includedClients "$($body.includedClients)"
+
+foreach(client in clientList){
+		$outputBody += "Found client $($client.displayName) with tenant ID $($client.tenantId) .`r`n"
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $outputBody
+})
