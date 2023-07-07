31a1bd978b6fc449cdb711431adf6bc42ce147bb
SIRKit-ltd <Licensing@SIRKit.ca>
2023-07-07 15:32:49 -0600

    Update run.ps1

--- ClientTest/run.ps1
+++ ClientTest/run.ps1
@@ -16,11 +16,9 @@ if ($Request.Query.allClients -eq 'true') { $allClientsBool = $true }
 #Get a filtered client list of only clients that should have code executed.
 $clientList = GetClientList -allClients $allClientsBool -excludedClients "$($body.excludedClients)" -includedClients "$($body.includedClients)"
 
-#foreach($client in $clientList){
-#		$outputBody += "Found client $($client.displayName) with tenant ID $($client.tenantId) .`r`n"
-#}
-
-$outputBody = $clientList
+foreach($client in $clientList){
+		$outputBody += "Found client $($client.displayName) with tenant ID $($client.tenantId) .`r`n"
+}
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
